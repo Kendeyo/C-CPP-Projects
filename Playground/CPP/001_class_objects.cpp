a84473fd3b8e2716e3455e83561c4333ccdd515d
Kennedy Odeyo <101339620+Kendeyo@users.noreply.github.com>
2025-05-10 21:34:54 +0300

    create class objects

---
+++ Playground/CPP/001_class_objects.cpp
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+
+class MyClass {       // The class
+    public:             // Access specifier
+      int myNum;        // Attribute (int variable)
+      string myString;  // Attribute (string variable)
+  };
+  
+  int main() {
+    MyClass myObj;  // Create an object of MyClass
+  
+    // Access attributes and set values
+    myObj.myNum = 15; 
+    myObj.myString = "Some text";
+  
+    // Print attribute values
+    cout << myObj.myNum << "\n";
+    cout << myObj.myString;
+    cin.get(); // Wait for user input before closing the console window
+    return 0;
+  }
