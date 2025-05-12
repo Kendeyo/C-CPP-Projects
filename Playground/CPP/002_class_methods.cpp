d02ca3c52234c29d1609fd21d79da9ee0777d0dc
Kennedy Odeyo <101339620+Kendeyo@users.noreply.github.com>
2025-05-12 21:55:17 +0300

    update-added comments

--- Playground/CPP/002_class_methods.cpp
+++ Playground/CPP/002_class_methods.cpp
@@ -1,3 +1,14 @@
+/**
+ * Methods are functions that belongs to the class.
+ * There are two ways to define functions that belongs to a class:
+ *    Inside class definition
+ *    Outside class definition
+ * 
+ *  In the following example, we define a function inside the class, and we name it "myMethod".
+ * You access methods just like you access attributes; by creating an object of the class
+ *  and using the dot syntax (.):
+ */
+
 #include <string.h>
 #include <iostream>
 using namespace std;
